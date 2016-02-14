3cabd6f0bca16748e5a1e98f3fe6f0a64001fa6a
pavelmp <pavelmp@gmail.com>
2016-02-13 19:04:53 -0800

    Implement BMR and make newly created link appear on Shorten page

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,18 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+  $scope.link = {};
+
+  $scope.addLink = function () {
+    Links.addOne($scope.link);
+    Links.getAll().then(function (links) {
+      $scope.oneLink = links.filter(function (v) {
+        return v.url === $scope.link.url;
+      })[0];
+      $scope.showMessage = true;
+      $scope.link.url = '';
+    });
+  };
+
+  $scope.showMessage = false;
 });
